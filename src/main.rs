6a8d675f0e8a930bf6c126587b4171ab03dfdfb2
Hajime Fujita <crisp.fujita@nifty.com>
2022-03-06 04:10:38 -0500

    Remove extra debug print

--- src/main.rs
+++ src/main.rs
@@ -398,7 +398,6 @@ mod daikin {
         }
 
         fn do_set_setpoints(&self, heat: f64, cool: f64, duration: u32) -> Result<(), Error> {
-            println!("Setting setpoints: heat={:.1}, cool={:.1}", heat, cool);
             let url = format!("https://api.daikinskyport.com/deviceData/{}", self.device_id);
             let body = format!("{{\"hspHome\": {:.1}, \"cspHome\": {:.1}, \"schedOverride\": 1, \"schedOverrideDuration\": {}}}",
                 heat, cool, duration);
